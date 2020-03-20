24054a51575b24e6749ab470cad72a634c818701
Luca <hohmann.luca@tutanota.com>
2020-03-20 23:01:52 +0100

    Writing to server, pre file data, indexing Added: 1) writing to server - clients now send the file to the server, which writes it into memory
    2) Client has to send some information to the server ahead of the file,
    e.g. file name and whether the file should go to phone or pc
    
    3) server indexes the received files with the destination so later when
    send the file to the destination, it knows which files to send.

--- Image_Sync_Server/src/client.py
+++ Image_Sync_Server/src/client.py
@@ -3,15 +3,24 @@ import socket
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 2222        # The port used by the server
 
-file = open("E:/a.txt", "wb")
+file = open("../test.txt", "rb")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    data = s.recv(1024)
+    #data = s.recv(1024)
 
+    #startbytes
+    s.send(b'\x00\x03\x02')
+    s.send(b"test.txt")
+
+    data = file.read(1024)
     while data:
-        print("i")
-        file.write(data)
-        data = s.recv(1024)
-    file.close()
+        s.sendall(data)
+        data = file.read(1024)
+    print("Sending done")
+    # while data:
+    #     print("i")
+    #     file.write(data)
+    #     data = s.recv(1024)
+    # file.close()
 
-print('Received', repr(data))
+#print('Received', repr(data))
--- Image_Sync_Server/src/server.py
+++ Image_Sync_Server/src/server.py
@@ -6,21 +6,44 @@ serversocket.bind(('localhost', 2222))
 
 serversocket.listen(5)
 
+
+
 file = open("D:/a.txt", mode='rb')
-filesize = os.path.getsize("D:/config.jar")
+
+counter = 0
 
 BUFF_SIZE = 1024
 
 while True:
     (clientsocket, address) = serversocket.accept()
     with clientsocket:
-        print("Connected by ", address)
-        print('Connected by', clientsocket)
-        counter = 0
-        data = file.read(BUFF_SIZE)
+        index_file = open("../files/fileindex.txt", mode="a")
+        # [are you pc or phone, send to phone/pc, how many images sent]
+        client_auth = clientsocket.recv(3)
+        print("Client auth: ", client_auth[0])
+        print("Client auth: ", client_auth[1])
+        print("Client auth: ", client_auth[2])
+
+        # client will then send all of the file names (max length 20 with file type) so that the server can create these intermediate
+        # filenames = clientsocket.recv(20*client_auth[2])
+
+        filenames = clientsocket.recv(20)
+        print("File name:", filenames.decode("utf-8"))
+        newWriteFile = open("../files/"+filenames.decode("utf-8"), mode='wb')
+
+        data = clientsocket.recv(1024)
         while data:
-            print(counter)
-            counter += 1
-            clientsocket.sendall(data)
-            data = file.read(BUFF_SIZE)
+            print("data:", data)
+            newWriteFile.write(data)
+            data = clientsocket.recv(1024)
+        newWriteFile.close()
+        print("Writing done")
+
+        index_file.write(str(client_auth[1]) + filenames.decode("utf-8")+'\n')
+        index_file.close()
+        #
+        # data = file.read(BUFF_SIZE)
+        # while data:
+        #     clientsocket.sendall(data)
+        #     data = file.read(BUFF_SIZE)
         clientsocket.close()
